46dea0b000e44e6b807717432bb74ff2cc79e6a3
BugHunter-X <askzenai@gmail.com>
2025-02-08 21:52:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,10 @@
+def my_function(a, b):
+    if b == 0:
+        return "Division by zero is not allowed"
+    else:
+        return a / b
+
+result = my_function(10, 0)
+print(result)
+result2 = my_function(10,2)
+print(result2)
